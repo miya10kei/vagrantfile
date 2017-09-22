bfa627b13709e0af6242397692a770e9efa8d23d
miya10kei <miya10kei@gmail.com>
2017-09-23 04:17:59 +0900

    add export nodejs's env var.

--- provisioning/setup-nodejs.sh
+++ provisioning/setup-nodejs.sh
@@ -5,10 +5,13 @@ echo "###################"
 echo "# Install Node.js #"
 echo "###################"
 
-if [ -e $HOME/.nvm ]; then
+if [ -e .nvm ]; then
   echo "Node.js is already installed."
 else
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
   nvm install --lts
   nvm use --lts
   echo "Node.js install completed."
