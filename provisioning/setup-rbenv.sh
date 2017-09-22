dab28c030751e49a441e5ca4dec1a03509cf6119
miya10kei <miya10kei@gmail.com>
2017-09-23 03:50:27 +0900

    add sudo to apt-get command.

--- provisioning/setup-rbenv.sh
+++ provisioning/setup-rbenv.sh
@@ -8,7 +8,7 @@ echo "#################"
 if [ -e .rbenv ]; then
   echo "rbenv is already installed."
 else
-  apt-get install -y build-essential libssl-dev ruby-build
+  sudo apt-get install -y build-essential libssl-dev ruby-build
   git clone https://github.com/rbenv/rbenv.git ~/.rbenv
   git clone https://github.com/sstephenson/ruby-build.git .rbenv/plugins/ruby-build
   echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
